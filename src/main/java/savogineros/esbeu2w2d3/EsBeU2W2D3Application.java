4410ba0616bad347febc5b20d78607bf4aab2ca6
Eros Savogin <eros.sav@gmail.com>
2024-01-17 16:49:15 +0100

    Initial commit

---
+++ src/main/java/savogineros/esbeu2w2d3/EsBeU2W2D3Application.java
@@ -0,0 +1,13 @@
+package savogineros.esbeu2w2d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsBeU2W2D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsBeU2W2D3Application.class, args);
+	}
+
+}
---
+++ src/test/java/savogineros/esbeu2w2d3/EsBeU2W2D3ApplicationTests.java
@@ -0,0 +1,13 @@
+package savogineros.esbeu2w2d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsBeU2W2D3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
